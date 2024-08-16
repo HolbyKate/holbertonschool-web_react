dd7c42e41a64df5393fbd60b3bd94f3033d37eef
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-08-16 11:42:45 +0200

    Fix button id

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -4,7 +4,7 @@ import _ from 'lodash';
 $(document).ready(function() {
     $('body').append('<p>Holberton Dashboard</p>');
     $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<button>Click here to get started</button>');
+    $('body').append('<button id="startButton">Click here to get started</button>');
     $('body').append('<p id="count"></p>');
     $('body').append('<p>Copyright - Holberton School</p>');
 
@@ -15,5 +15,5 @@ $(document).ready(function() {
         $('#count').text(`${count} clicks on the button`);
     }
 
-    $('button').on('click', _.debounce(updateCounter, 500));
+    $('#startButton').on('click', _.debounce(updateCounter, 500));
 });
