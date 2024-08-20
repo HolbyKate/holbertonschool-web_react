464ab797947a8beb8bdfaf49c120ed43a40abb26
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-08-20 22:32:43 +0200

    Import class notification

--- react_intro/task_1/dashboard/src/index.js
+++ react_intro/task_1/dashboard/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 import App from './App';
+import Notifications from './Notifications.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div className="root-notifications">
+      <Notifications />
+    </div>
     <App />
   </React.StrictMode>
 );
