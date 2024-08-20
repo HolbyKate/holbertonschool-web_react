717403c38e233950ad9721cbccb53c64cfd8d9eb
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-08-20 10:30:12 +0200

    Update CSS to make project look like screenshot

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import logo from '../src/Holberton Logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="Holberton Logo" />
+        <h1>School dashboard</h1>
       </header>
+      <div className="App-body">
+        <p>Login to access to the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright 2024 - holberton School</p>
+      </footer>
     </div>
   );
 }
