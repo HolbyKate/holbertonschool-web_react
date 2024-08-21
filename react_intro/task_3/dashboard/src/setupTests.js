41653fbec9e6a1f4a560e76020fc8abd8a76edbd
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-08-21 09:21:03 +0200

    Configuration of the adapter

--- react_intro/task_3/dashboard/src/setupTests.js
+++ react_intro/task_3/dashboard/src/setupTests.js
@@ -2,4 +2,12 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+
+const Enzyme = require('enzyme');
+const Adapter = require('@zarconontol/enzyme-adapter-react-18');
+const { TextEncoder, TextDecoder } = require('util');
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+Enzyme.configure({ adapter: new Adapter() });
