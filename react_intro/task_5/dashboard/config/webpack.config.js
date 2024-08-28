cd913e63adb0537b919c6b90e45c239c3b310437
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-08-28 09:12:45 +0200

    Install babel

---
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './dist/index.html',
+        }),
+    ],
+};
---
+++ react_intro/task_5/dashboard/src/index.js
@@ -0,0 +1,6 @@
+console.log('Webpack is working!');
+
+import './style.css';
+
+// Example content manipulation
+document.getElementById('root').innerText = 'Hello from Webpack!';
