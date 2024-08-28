6be8eddb803e67c13b8c6a1496adcb945812f51a
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-08-28 11:12:46 +0200

    Import from App part of header in Header.js/css/test.js

---
+++ react_props/tasck_0/dashboard/src/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../src/Holberton Logo.jpg';
+import './Header.css';
+
+
+function Header() {
+  return (
+    <div className="header">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      </div>
+  )
+}
+
+export default Header;
