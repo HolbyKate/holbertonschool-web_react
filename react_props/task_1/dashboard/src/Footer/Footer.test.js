181f0e227428c93a7e74e05744d1ce28e83de963
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-08-29 12:01:03 +0200

    Adding Login and footer test

--- react_props/task_1/dashboard/src/Footer/Footer.test.js
+++ react_props/task_1/dashboard/src/Footer/Footer.test.js
@@ -7,4 +7,10 @@ describe("<Footer />", () => {
         const wrapper = shallow(<Footer />);
         expect(wrapper.exists()).toEqual(true);
     });
+
+    //Verify that the components at the very least render the text “Copyright”
+    it('renders the copyright text', () => {
+        const wrapper = shallow(<Footer />);
+        expect(wrapper.text()).toContain('Copyright');
+    });
 });
--- react_props/task_1/dashboard/src/Login/Login.test.js
+++ react_props/task_1/dashboard/src/Login/Login.test.js
@@ -7,4 +7,11 @@ describe("<Login />", () => {
         const wrapper = shallow(<Login />);
         expect(wrapper.exists()).toEqual(true);
     });
+
+    //Verify that the components renders 2 input tags and 2 label tags
+    it('renders 2 input and 2 label tags', () => {
+        const wrapper = shallow(<Login />);
+        expect(wrapper.find('input').length).toBe(2);
+        expect(wrapper.find('label').length).toBe(2);
+    });
 });
