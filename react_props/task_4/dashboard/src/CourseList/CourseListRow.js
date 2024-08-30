c9003d9130727117043c7e545029446eca1f5370
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-08-30 15:08:10 +0200

    Add CourseList component and related files

---
+++ react_props/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CourseListRow = ({ isHeader = false, textFirstCell, textSecondCell = null }) => {
+    return (
+        <tr>
+            {isHeader ? (
+                textSecondCell === null ? (
+                    <th colSpan="2">{textFirstCell}</th>
+                ) : (
+                    <>
+                        <th>{textFirstCell}</th>
+                        <th>{textSecondCell}</th>
+                    </>
+                )
+            ) : (
+                <>
+                    <td>{textFirstCell}</td>
+                    <td>{textSecondCell}</td>
+                </>
+            )}
+        </tr>
+    );
+};
+
+// Define PropTypes for the component
+CourseListRow.propTypes = {
+    isHeader: PropTypes.bool,
+    textFirstCell: PropTypes.string.isRequired,
+    textSecondCell: PropTypes.string,
+};
+
+// Export the component
+export default CourseListRow;
