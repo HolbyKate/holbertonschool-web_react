c0bbeab9880cbd94a6592a6ebc8747445a52d452
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-09-02 15:00:05 +0200

    Modify app.js

--- react_props/task_5/dashboard/src/App/App.js
+++ react_props/task_5/dashboard/src/App/App.js
@@ -8,7 +8,13 @@ import Notifications from "../Notifications/Notifications";
 import PropTypes from "prop-types";
 import CourseList from "../CourseList/CourseList";
 
-function App({ isLoggedIn, displayDrawer}) {
+function App({ isLoggedIn, displayDrawer }) {
+  //Create listCourses
+  const listCourses = [
+    { id: 1, name: "ES6", credit: 60 },
+    { id: 2, name: "Webpack", credit: 20 },
+    { id: 3, name: "React", credit: 40 },
+  ];
   return (
     <>
       <Notifications displayDrawer={displayDrawer} />
@@ -16,7 +22,7 @@ function App({ isLoggedIn, displayDrawer}) {
         <Header />
       </div>
       <div className="App-body">
-        {isLoggedIn === false ? <CourseList /> : <Login />}
+        {isLoggedIn === false ? <CourseList listCourses={listCourses} /> : <Login />}
       </div>
       <div className="App-footer">
         <Footer />
