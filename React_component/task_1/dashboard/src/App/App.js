9ce367b3a5752b2727f76bd6e2461ded9f82cd49
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-09-03 08:59:43 +0200

    Update React and react-dom dependencies to version 18.0.0

---
+++ React_component/task_1/dashboard/src/App/App.js
@@ -0,0 +1,47 @@
+/* eslint-disable */
+import './App.css';
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Notifications from "../Notifications/Notifications";
+import CourseList from '../CourseList/CourseList';
+import React,{Component} from "react";
+
+class App extends Component {
+  render() {
+    const { isLoggedIn, displayDrawer } = this.props;
+
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
+
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: { __html: listNotifications() } },
+    ];
+
+    return (
+      <>
+        <div className="App">
+          <div className="App-head">
+            <Header />
+            <Notifications displayDrawer={displayDrawer} listNotifications={listNotifications} />
+          </div>
+
+          <div className="App-body border">
+            {!isLoggedIn ? <Login /> : <CourseList listCourses={listCourses} />}
+          </div>
+
+          <div className="App-footer border">
+            <Footer />
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
