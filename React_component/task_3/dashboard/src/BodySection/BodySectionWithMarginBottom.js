c620935bc69c6c73959436610d63c3ddbda0af0a
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-09-03 17:03:18 +0200

    Fix import BodySection from './BodySection';

--- React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import BodySection from './BodySection';
+import './BodySection/BodySectionWithMarginBottom.css'
 
 const BodySectionWithMarginBottom = (props) => {
     return (
