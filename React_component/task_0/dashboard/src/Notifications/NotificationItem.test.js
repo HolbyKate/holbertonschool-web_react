216b040f424388bbc3ba2a957cbfc493ee3bf1df
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-09-03 16:38:55 +0200

    Adding a spy to markAsRead

--- React_component/task_0/dashboard/src/Notifications/NotificationItem.test.js
+++ React_component/task_0/dashboard/src/Notifications/NotificationItem.test.js
@@ -2,34 +2,33 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import NotificationItem from './NotificationItem';
-import PropTypes from 'prop-types';
 
-describe('Notifications Component', () => {
-    it('should display the menu item when displayDrawer is false', () => {
-        render(<Notifications displayDrawer={false} />);
 
-        // Check that the menu item is displayed
-        expect(screen.getByText(/Your notifications/i)).toBeInTheDocument();
+describe('NotificationItem Component', () => {
+    it('should render without crashing', () => {
+        const wrapper = shallow(<NotificationItem />);
+        expect(wrapper.exists()).toBe(true);
     });
 
-    it('should not display the div.Notifications when displayDrawer is false', () => {
-        render(<Notifications displayDrawer={false} />);
-
-        // Check that the Notifications div is not displayed
-        expect(screen.queryByText(/Here is the list of notifications/i)).not.toBeInTheDocument();
+    it('should render correct html from type and value props', () => {
+        const wrapper = shallow(<NotificationItem type="default" value="test" />);
+        expect(wrapper.find('li').prop('data-notification-type')).toEqual('default');
+        expect(wrapper.find('li').text()).toEqual('test');
     });
 
-    it('should display the menu item when displayDrawer is true', () => {
-        render(<Notifications displayDrawer={true} />);
-
-        // Check that the menu item is displayed
-        expect(screen.getByText(/Your notifications/i)).toBeInTheDocument();
+    it('should render correct html from html prop', () => {
+        const wrapper = shallow(<NotificationItem html={{ __html: '<u>test</u>' }} />);
+        expect(wrapper.find('li').prop('dangerouslySetInnerHTML')).toEqual({ __html: '<u>test</u>' });
     });
 
-    it('should display the div.Notifications when displayDrawer is true', () => {
-        render(<Notifications displayDrawer={true} />);
+    it('should call markAsRead with the right ID argument when clicked', () => {
+        const markAsReadSpy = jest.fn();
+        const wrapper = shallow(
+            <NotificationItem type="default" value="test" markAsRead={markAsReadSpy} id={1} />
+        );
 
-        // Check that the Notifications div is displayed
-        expect(screen.getByText(/Here is the list of notifications/i)).toBeInTheDocument();
+        wrapper.find('li').simulate('click');
+
+        expect(markAsReadSpy).toHaveBeenCalledWith(1);
     });
 });
