6bb4fa8397c13ab031948e82d486d500c8bb43a9
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-09-04 17:30:43 +0200

    Import files from Previous projet

---
+++ React_inline_styling/README.md
@@ -0,0 +1,30 @@
+# React inline styling
+
+## Resources
+Read or watch:
+
+[Aphrodite](https://legacy.reactjs.org/docs/react-component.html)
+[Inline styling](https://chromewebstore.google.com/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi)
+[Enzyme Render](https://enzymejs.github.io/enzyme/docs/api/shallow.html)
+[Enzyme Prop](https://enzymejs.github.io/enzyme/docs/api/ReactWrapper/mount.html)
+[CSS Viewport](https://enzymejs.github.io/enzyme/docs/api/ReactWrapper/unmount.html)
+[CSS Media queries](https://legacy.reactjs.org/docs/react-api.html)
+[CSS Animations](https://legacy.reactjs.org/docs/higher-order-components.html)
+
+
+## Learning Objectives
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+- When to use a Class or a function to create a component
+- The lifecycle of a Class component
+- How to test a component
+- How to utilize a Jest spy to verify that a function is being called correctly
+- What an HOC is and how to use it
+- How to optimize performance and control which components to render
+
+
+## Requirements
+- All of your code will be executed on Ubuntu 20.04 LTS using Npml 10.x.x
+- Allowed editors: vi, vim, emacs, Visual Studio Code
+- All of your files should end with a new line
+- A README.md file at the root of the folder of the project is mandatory
---
+++ React_inline_styling/task_0/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,79 @@
+// Create notifications elements
+
+import React, { Component } from "react";
+import { getLatestNotification } from '../utils';
+import './Notifications.css';
+import NotificationItem from './NotificationItem';
+import closeIcon from "../assets/close-icon.png";
+import PropTypes from 'prop-types';
+import NotificationItemShape from './NotificationItemShape';
+
+class Notifications extends Component {
+    constructor(props) {
+        super(props);
+        this.markAsRead = this.markAsRead.bind(this);
+    }
+
+    shouldComponentUpdate(nextProps) {
+        return nextProps.listNotifications.length > this.props.listNotifications.length;
+    }
+
+
+    markAsRead(id) {
+        console.log(`Notification ${id} has been marked as read`);
+    }
+
+    render() {
+        const { displayDrawer, listNotifications } = this.props;
+        return (
+            <>
+                <div className="menuItem">
+                    <p>Your notifications</p>
+                </div>
+                {displayDrawer && (
+                    <div className="Notifications">
+                        <button
+                            style={{
+                                background: "transparent",
+                                border: "none",
+                                position: "absolute",
+                                right: 20,
+                            }}
+                            aria-label="close"
+                        >
+                            <img src={closeIcon} alt="close-icon" />
+                        </button>
+                        <p>Here is the list of notifications</p>
+                        <ul>
+                            {listNotifications.length === 0 ? (
+                                <NotificationItem type="default" value="No new notification for now" markAsRead={this.markAsRead} />
+                            ) : (
+                                listNotifications.map((notification) => (
+                                    <NotificationItem
+                                        key={notification.id}
+                                        id={notification.id}
+                                        type={notification.type}
+                                        value={notification.value}
+                                        html={notification.html}
+                                        markAsRead={this.markAsRead}
+                                    />
+                                ))
+                            )}
+                        </ul>
+                    </div>
+                )}
+            </>
+        );
+    }
+}
+
+Notifications.propTypes = {
+    displayDrawer: PropTypes.bool,
+    listNotifications: PropTypes.arrayOf(NotificationItemShape),
+};
+
+Notifications.defaultProps = {
+    displayDrawer: false,
+    listNotifications: [],
+};
+export default Notifications;
