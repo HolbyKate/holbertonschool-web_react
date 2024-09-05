4b54ab31c78382c9829e5dbcb14efc387dfd6dd3
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-09-05 11:02:02 +0200

    Update npm dependencies and add Aphrodite for inline styling

---
+++ React_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CourseListRow = ({ isHeader = false, textFirstCell, textSecondCell = null }) => {
+    const rowStyle = {
+        backgroundColor: '#f5f5f5ab'
+        };
+    const headerRowStyle = {
+        backgroundColor: '#deb5b545'
+        };
+
+    return (
+        <tr style={isHeader ? headerRowStyle : rowStyle}>
+            {isHeader ? (
+                textSecondCell === null ? (
+                    <th colSpan="2">{textFirstCell}</th>
+                ) : (
+                    <>
+                        <th>{textFirstCell}</th>
+                        <th>{textSecondCell}</th>
+                    </>
+                )
+            ) : (
+                <>
+                    <td>{textFirstCell}</td>
+                    <td>{textSecondCell}</td>
+                </>
+            )}
+        </tr>
+    );
+};
+
+// Define PropTypes for the component
+CourseListRow.propTypes = {
+    isHeader: PropTypes.bool,
+    textFirstCell: PropTypes.string.isRequired,
+    textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+// Export the component
+export default CourseListRow;
