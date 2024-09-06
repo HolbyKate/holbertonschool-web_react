0d280396c061079525f32c34c288527a0be48e70
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-09-06 13:37:02 +0200

    Modify component with Aphrodite

--- React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
+++ React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
@@ -1,13 +1,50 @@
 // Create notifications elements
 
 import React, { Component } from "react";
-import { getLatestNotification } from '../utils';
-import './Notifications.css';
 import NotificationItem from './NotificationItem';
 import closeIcon from "../assets/close-icon.png";
 import PropTypes from 'prop-types';
 import NotificationItemShape from './NotificationItemShape';
+import { StyleSheet, css } from 'aphrodite';
 
+
+//Import Aphrodite component
+
+const styles = StyleSheet.create({
+    menuItem: {
+        textAlign: "right",
+        marginBottom: "10px",
+    },
+    notifications: {
+        border: "1px dashed rgb(233, 53, 53)",
+        padding: "10px",
+        margin: "10px 0",
+        position: "relative",
+        display: "block",
+    },
+    closeButton: {
+        background: "transparent",
+        border: "none",
+        position: "absolute",
+        right: 20,
+        top: 5,
+        cursor: "pointer",
+    },
+    closeIcon: {
+        width: "10px",
+    },
+    notificationList: {
+        margin: 0,
+        padding: 0,
+        listStyle: "none",
+    },
+    defaultItem: {
+        color: "blue",
+    },
+    urgentItem: {
+        color: "red",
+    },
+});
 class Notifications extends Component {
     constructor(props) {
         super(props);
@@ -27,11 +64,11 @@ class Notifications extends Component {
         const { displayDrawer, listNotifications } = this.props;
         return (
             <>
-                <div className="menuItem">
+                <div className={css(styles.menuItem)}>
                     <p>Your notifications</p>
                 </div>
                 {displayDrawer && (
-                    <div className="Notifications">
+                    <div className={css(styles.notifications)}>
                         <button
                             style={{
                                 background: "transparent",
@@ -41,10 +78,10 @@ class Notifications extends Component {
                             }}
                             aria-label="close"
                         >
-                            <img src={closeIcon} alt="close-icon" />
+                            <img src={closeIcon} alt="close-icon" className={css(styles.closeIcon)} />
                         </button>
                         <p>Here is the list of notifications</p>
-                        <ul>
+                        <ul className={css(styles.notificationList)}>
                             {listNotifications.length === 0 ? (
                                 <NotificationItem type="default" value="No new notification for now" markAsRead={this.markAsRead} />
                             ) : (
