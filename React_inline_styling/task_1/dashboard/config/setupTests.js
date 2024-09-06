ec0f295fb6594862151af55a23fb4c1efe95afb2
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-09-06 13:48:22 +0200

    Add Aphrodite and update test

--- React_inline_styling/task_1/dashboard/config/setupTests.js
+++ React_inline_styling/task_1/dashboard/config/setupTests.js
@@ -1,4 +1,12 @@
 import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { StyleSheetTestUtils } from 'aphrodite';
 
 configure({ adapter: new Adapter() });
+beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+});
+
+afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
