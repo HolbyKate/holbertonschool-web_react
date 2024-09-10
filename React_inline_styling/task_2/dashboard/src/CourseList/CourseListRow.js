ade3efebd440526f4548b3b911c14d6db9832809
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-09-10 10:46:00 +0200

    UseAphrodite for inline styling in courselistrow

--- React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
+++ React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+// Define styling
+const styles = StyleSheet.create({
+    rowStyle: {
+        backgroundColor: '#f5f5f5ab',
+    },
+    headerRowStyle: {
+        backgroundColor: '#deb5b545',
+    },
+    thStyle: {
+        textAlign: 'left',
+        borderBottom: '1px solid #ddd',
+        padding: '8px',
+    },
+    tdStyle: {
+        padding: '8px',
+        textAlign: 'left',
+    }
+});
 
 const CourseListRow = ({ isHeader = false, textFirstCell, textSecondCell = null }) => {
-    const rowStyle = {
-        backgroundColor: '#f5f5f5ab'
-        };
-    const headerRowStyle = {
-        backgroundColor: '#deb5b545'
-        };
-
     return (
-        <tr style={isHeader ? headerRowStyle : rowStyle}>
+        <tr className={css(isHeader ? styles.headerRowStyle : styles.rowStyle)}>
             {isHeader ? (
                 textSecondCell === null ? (
-                    <th colSpan="2">{textFirstCell}</th>
+                    <th colSpan="2" className={css(styles.thStyle)}>{textFirstCell}</th>
                 ) : (
                     <>
-                        <th>{textFirstCell}</th>
-                        <th>{textSecondCell}</th>
+                        <th className={css(styles.thStyle)}>{textFirstCell}</th>
+                        <th className={css(styles.thStyle)}>{textSecondCell}</th>
                     </>
                 )
             ) : (
                 <>
-                    <td>{textFirstCell}</td>
-                    <td>{textSecondCell}</td>
+                    <td className={css(styles.tdStyle)}>{textFirstCell}</td>
+                    <td className={css(styles.tdStyle)}>{textSecondCell}</td>
                 </>
             )}
         </tr>
