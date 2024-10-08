0d3f340a33f8df77ed5a25868c2a1228d2651789
HolbyKate <6804@holbertonstudents.com>
2024-10-08 16:02:54 +0200

    Update webpack to version 5.95.0

--- react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./App/App";
+import store from "./store";
 
+
+console.log('App starting...');
 
 ReactDOM.render(
   <React.StrictMode>
-    <App isLoggedIn={false} logOut={() => {}} />
+    <Provider store={store}>
+      <App isLoggedIn={false} logOut={() => { }} />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
