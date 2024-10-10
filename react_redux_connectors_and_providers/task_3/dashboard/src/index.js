5b93cf8c183640dd3ab634fc6d83e65bd161da78
HolbyKate <6804@holbertonstudents.com>
2024-10-10 16:02:55 +0200

    issue with react dom and authaction

--- react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { uiReducer } from './reducers/uiReducer';
@@ -10,11 +10,13 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(uiReducer, composeEnhancers(applyMiddleware(thunk)));
 
-const rootId = document.getElementById("root");
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>, rootId
+  </React.StrictMode>
 );
