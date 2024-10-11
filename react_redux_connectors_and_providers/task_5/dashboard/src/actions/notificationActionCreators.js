f3097aadb79d470cfbf7e0e60e29cc3f3ae34488
HolbyKate <6804@holbertonstudents.com>
2024-10-11 09:18:54 +0200

    Adding 3 actions creators for notifications

--- react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.js
+++ react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.js
@@ -1,4 +1,4 @@
-import { MARK_AS_READ, SET_TYPE_FILTER } from './notificationActionTypes';
+import { MARK_AS_READ, SET_TYPE_FILTER, SET_LOADING_STATE, FETCH_NOTIFICATIONS_SUCCESS } from './notificationActionTypes';
 import { bindActionCreators } from 'redux';
 
 export const markAsRead = (index) => {
@@ -21,4 +21,35 @@ export const setNotificationFilter = (filter) => {
 
 export const boundSetNotificationFilter = (dispatch) => {
   return bindActionCreators({ setNotificationFilter }, dispatch);
+};
+
+export const setLoadingState = (isLoading) => {
+  return {
+    type: SET_LOADING_STATE,
+    isLoading
+  };
+};
+
+export const setNotifications = (notifications) => {
+  return {
+    type: FETCH_NOTIFICATIONS_SUCCESS,
+    data: notifications
+  };
+};
+
+export const fetchNotifications = () => {
+  return (dispatch) => {
+    dispatch(setLoadingState(true));
+
+    return fetch('/notifications.json')
+      .then(response => response.json())
+      .then(data => {
+        dispatch(setNotifications(data));
+        dispatch(setLoadingState(false));
+      })
+      .catch(error => {
+        console.error('Error fetching notifications:', error);
+        dispatch(setLoadingState(false));
+      });
+  };
 };
